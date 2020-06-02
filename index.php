75ba70a8efe0c04b6be6b56b51d67e11daa1e6ac
Olu <theoafactor@gmail.com>
2020-06-02 13:38:49 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home | Create Modules From PDFs</title>
+	<!-- CSS only -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+
+<div class="container">
+	<h3><strong>Upgrade Basecamp today and run your business better.</strong></h3>
+	<h6>Pick a plan below and join over <strong>120,000 companies</strong> that upgrade their Basecamp.</h6>
+	<div class="card shadowed" style="padding: 20px 0px;">
+		<h5><strong>Which payment option fits you best?</strong></h5>
+		<div class="row" style="text-align: center; display: block; margin: auto;">
+			<div class="col" style="max-width: 85%; margin: auto;">
+				<div class="row" style="padding: 10px;">
+					<div class="col-md">
+						<div class="card inner" style="border: 2px solid #725EAD; background-color: #F9F9F9;">
+							<h5><strong>Pay annually</strong></h5>
+							<button class="button" style="margin: 5px 0px;">Choose this plan</button>
+							<div class="price-list" style="margin-top: 10px;">
+								<ul>
+									<li><i class="fa fa-check-circle" style="color: #725EAD;"></i><strong> $999/year</strong> - 2 months free</li>
+									<li><i class="fa fa-check-circle" style="color: #725EAD;"></i> 1TB storage</li>
+									<li><i class="fa fa-check-circle" style="color: #725EAD;"></i> 1 year money back guarantee</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="col-md">
+						<div class="card inner">
+							<h5><strong>Pay monthly</strong></h5>
+							<button class="button" style="margin: 5px 0px;">Choose this plan</button>
+							<div class="price-list" style="margin-top: 10px;">
+								<ul>
+									<li><i class="fa fa-check-circle" style="color: #725EAD;"></i><strong> $99/month</strong></li>
+									<li><i class="fa fa-check-circle" style="color: #725EAD;"></i> 500GB storage</li>
+									<li><i class="fa fa-check-circle" style="color: #725EAD;"></i> Cancel any time</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<p style="color: #8D7A5D;">included with your upgrade:<br/>Unlimited Projects and Teams &#9679; Unlimited users &#9679; No Per-User Fees</p>
+	</div>
+</div>
+
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+</body>
+</html>
